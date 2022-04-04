d8f12cebc02b28fc8636021edb5e3f7e02d2c30d
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2022-04-04 12:49:02 +0100

    - Check documentation for each function - Code formatting to match 'black' style - Some code optimization - Some renaming of "BDM" to "bond distortions" etc

--- defect_finder/distortions.py
+++ defect_finder/distortions.py
@@ -1,175 +1,217 @@
 """
-Code with defetc distortion functions (bdm, rattle and local_rattle)
-
+Module containing functions for applying distortions to defect structures
 """
 from typing import Optional
 
 import numpy as np
-from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.core.structure import Structure
-from pymatgen.transformations.advanced_transformations import MonteCarloRattleTransformation
+from pymatgen.io.ase import AseAtomsAdaptor
+
+# TODO: from pymatgen.transformations.advanced_transformations import MonteCarloRattleTransformation
+
 
 def bdm(
     structure: Structure,
     num_nearest_neighbours: int,
     distortion_factor: float,
-    atom_index: int = None, # starting from 1
-    frac_coords: np.ndarray = None, # if it's a vacancy, use frac coords instead of site index (as a numpy array)
-    verbose: bool = False,
+    site_index: Optional[int] = None,  # starting from 1
+    frac_coords: Optional[np.array] = None,  # use frac coords for vacancies
     distorted_element: Optional[str] = None,
+    verbose: Optional[bool] = False,
 ) -> dict:
-    """Applies a bond distortion factor to num_nearest_neighbours of defect (indicated with atom_index (antisites/interstitials) or frac coordinates (vacancies))
+    """
+    Applies bond distortions to `num_nearest_neighbours` of the defect (specified by
+    `site_index` (for substitutions or interstitials) or `frac_coords`  (for vacancies))
+
     Args:
-        structure (Structure): 
-            defect structure in pymatgen format
-        num_nearest_neighbours (int): 
-            number of defect neighbours to apply distortion
-        distortion factor (float): 
-            bond distortion factor to apply to bond distance between defects and defect nearest neighbours. 
-            Often good performance obtained with factors between 0.4 (-60%) and 1.4 (+60%)
-        atom_index (int) : 
-            atom index of defect, starting from 1 (for antisites or interstitials)
-        frac_coords (np.array): 
-            for vacancies.
-        distorted_element (str): 
-            Allows the user to specify the neighbouring element to distort. If None, the closest neighbours to the defect will be chosen.
-            (default: None)
-        verbose (bool) :
-            whether to print distortion information
-            (default: False)
+        structure (:obj:`~pymatgen.core.structure.Structure`):
+            Defect structure as a pymatgen object
+        num_nearest_neighbours (:obj:`int`):
+            Number of defect nearest neighbours to apply bond distortions to
+        distortion factor (:obj:`float`):
+            The distortion factor to apply to the bond distance between the defect and nearest
+            neighbours. Typical choice is between 0.4 (-60%) and 1.4 (+60%).
+        site_index (:obj:`int`, optional):
+            Index of defect site in structure (for substitutions or interstitials), counting from 1
+        frac_coords (:obj:`numpy.ndarray`, optional):
+            Fractional coordinates of the defect site in the structure (for vacancies)
+        distorted_element (:obj: `str`, optional):
+            Neighbouring element to distort. If None, the closest neighbours to the defect will
+            be chosen. (default: None)
+        verbose (:obj:`bool`, optional):
+            Whether to print distortion information. (default: False)
+
     Returns:
-        dictionary with distorted defect structure and parameters used in the distortion"""
+        Dictionary with distorted defect structure and the distortion parameters.
+        """
     aaa = AseAtomsAdaptor()
     input_structure_ase = aaa.get_atoms(structure)
 
-    if atom_index:
-        atom_number = atom_index - 1  ## Align atom number with python indexing
-    elif isinstance(frac_coords, np.ndarray): ## Only for vacancies!
-        input_structure_ase.append("V") ## fake "V" at vacancy
+    if site_index:
+        atom_number = site_index - 1  # Align atom number with python 0-indexing
+    elif isinstance(frac_coords, np.ndarray):  # Only for vacancies!
+        input_structure_ase.append("V")  # fake "V" at vacancy
         input_structure_ase.positions[-1] = np.dot(
             frac_coords, input_structure_ase.cell
         )
         atom_number = len(input_structure_ase) - 1
     else:
-        print("Where are my atom coordinates or index?")
+        raise ValueError(
+            "Insufficient information to apply bond distortions, no `site_index` or "
+            "`frac_coords` provided."
+        )
 
     neighbours = (
-            num_nearest_neighbours + 1
-        )  ## Prevent self counting of nearest neighbours
-    frac_positions = (
-        input_structure_ase.get_scaled_positions()
-    )  ## Get fractional atomic coordinates
-    distances = [
-        ( round(input_structure_ase.get_distance(atom_number, index, mic=True), 4), 
-         index + 1, symbol)
-        for index, symbol in zip( list(range(len(input_structure_ase))), 
-                                 input_structure_ase.get_chemical_symbols() 
-                                 )
-    ]  ## Get all distances between the selected atom and all other atoms
-    distances = sorted(distances, key=lambda tup: tup[0]) ## Sort the distances shortest->longest
-    
-    if distorted_element : 
+        num_nearest_neighbours + 1
+    )  # Prevent self-counting of the defect atom itself
+    distances = [  # Get all distances between the selected atom and all other atoms
+        (
+            round(input_structure_ase.get_distance(atom_number, index, mic=True), 4),
+            index + 1,
+            symbol,
+        )
+        for index, symbol in zip(
+            list(range(len(input_structure_ase))),
+            input_structure_ase.get_chemical_symbols(),
+        )
+    ]
+    distances = sorted(  # Sort the distances shortest->longest
+        distances, key=lambda tup: tup[0]
+    )
+
+    if (
+        distorted_element
+    ):  # filter the neighbours that match the element criteria and are
+        # closer than 4.5 Angstroms
         nearest = []
-        ## filter the neighbours that match the element criteria and are closer than 4.5 A
         for i in distances[1:]:
-            if len(nearest) < (neighbours - 1) and i[2] == distorted_element and i[0] < 4.5 : 
-                    nearest.append(i)
+            if (
+                len(nearest) < (neighbours - 1)
+                and i[2] == distorted_element
+                and i[0] < 4.5
+            ):
+                nearest.append(i)
             elif len(nearest) == (neighbours - 1):
                 break
-        ## if the number of nearest neighbours not reached, add other neighbouring elements 
-        [nearest.append(i) for i in distances[1:] if len(nearest) < (neighbours - 1) and i not in nearest and i[0]< 4.5] 
-    else:        
-        nearest = distances[1:neighbours]  ## Extract the nearest neighbours according to distance
+        # if the number of nearest neighbours not reached, add other neighbouring elements
+        while len(nearest) < (neighbours - 1):
+            for i in distances[1:]:
+                if i not in nearest and i[0] < 4.5:
+                    nearest.append(i)
+    else:
+        nearest = distances[
+            1:neighbours
+        ]  # Extract the nearest neighbours according to distance
     distorted = [
         (i[0] * distortion_factor, i[1], i[2]) for i in nearest
-    ]  ## Distort the nearest neighbour distances according to sf
-    [
-        input_structure_ase.set_distance(atom_number, i[1] - 1, i[0], fix=0, mic=True)
-        for i in distorted
-    ]  ## Modify the ase.Atoms object for the distorted case
-    
+    ]  # Distort the nearest neighbour distances according to the distortion factor
+    for i in distorted:
+        input_structure_ase.set_distance(
+            atom_number, i[1] - 1, i[0], fix=0, mic=True
+        )  # Set the distorted distances in the ASE Atoms object
+
     if isinstance(frac_coords, np.ndarray):
-        input_structure_ase.pop(-1) # remove fake V
-    
+        input_structure_ase.pop(-1)  # remove fake V from vacancy structure
+
     distorted_structure = aaa.get_structure(input_structure_ase)
-    distorted_atoms =  [(i[1], i[2]) for i in nearest ] # element and site number
+    distorted_atoms = [(i[1], i[2]) for i in nearest]  # element and site number
+
     # Create dictionary with distortion info & distorted structure
     bdm_distorted_defect = {
         "distorted_structure": distorted_structure,
-        "defect_index": atom_index or None,
         "number_distorted_neighbours": num_nearest_neighbours,
-        "distorted_atoms": distorted_atoms 
-        }
+        "distorted_atoms": distorted_atoms,
+        "undistorted_structure": structure,
+    }
+    if site_index:
+        bdm_distorted_defect["defect_site_index"] = site_index
+    elif frac_coords:
+        bdm_distorted_defect["defect_frac_coords"] = frac_coords
+
     if verbose:
-        distorted = [ (round(i[0],2), i[1], i[2]) for i in distorted ] ; nearest = [ (round(i[0],2), i[1], i[2]) for i in nearest ] # round numbers 
-        print("     Defect Atom Index/frac. coords:", atom_index or frac_coords )
-        print("     Original Neigbhour Distances:", nearest)
-        print("     Distorted Neighbour Distances:", distorted) ; print() 
+        distorted = [(round(i[0], 2), i[1], i[2]) for i in distorted]
+        nearest = [(round(i[0], 2), i[1], i[2]) for i in nearest]  # round numbers
+        print("     Defect Site Index / Frac Coords:", site_index or frac_coords)
+        print("     Original Neighbour Distances:", nearest)
+        print("     Distorted Neighbour Distances:\n", distorted)
 
-    return bdm_distorted_defect 
+    return bdm_distorted_defect
 
-def rattle(
-        structure: Structure, 
-        stdev: float = 0.25
-        ) -> Structure:
+
+def rattle(structure: Structure, stdev: Optional[float] = 0.25) -> Structure:
     """
-    Given a pymnatgen structure, it applies a random distortion to the coordinates of all atoms. 
-    Random distortion chosen from a gaussian with a standard deviation of stdev.
+    Given a pymatgen Structure object, apply random displacements to all atomic positions,
+    with the displacement distances randomly drawn from a Gaussian distribution of standard
+    deviation `stdev`.
+
     Args:
-        structure (Structure): pymatgen structure
-        stdev (float): 
-            standard dev of the gaussian used for rattle (in A)
+        structure (:obj:`~pymatgen.core.structure.Structure`):
+            Structure as a pymatgen object
+        stdev (:obj:`float`, optional):
+            Standard deviation (in Angstroms) of the Gaussian distribution from which atomic
+            displacement distances are drawn.
             (default: 0.25)
+
     Returns:
-        rattled structure
+        Rattled Structure object
     """
     aaa = AseAtomsAdaptor()
     ase_struct = aaa.get_atoms(structure)
     ase_struct.rattle(stdev=stdev)
     rattled_structure = aaa.get_structure(ase_struct)
-    
+
     return rattled_structure
 
+
 def localized_rattle(
-        structure: Structure, 
-        defect_coords: np.array,
-        stdev: float = 0.25,
-        ):
+    structure: Structure,
+    defect_coords: np.array,
+    cutoff: Optional[float] = 5.0,
+    stdev: Optional[float] = 0.25,
+):
     """
-    Given a pymnatgen structure, it applies a random distortion to the coordinates of the atoms in a radius 5 A from defect atom. 
-    Random distortion chosen from a gaussian with a standard deviation of stdev.
+    Given a pymatgen Structure object, apply random displacements to all atomic positions within
+    a specified radius in Angstroms (default 5) from the defect site, with the displacement
+    distances randomly drawn from a Gaussian distribution of standard deviation `stdev`.
+
     Args:
-        structure : 
-            Structure
-        defect_coords (np.array):
-            cartesian coordinates of defect
-        stdev (float): 
-            standard dev of the gaussian used for rattle (in A)
+        structure (:obj:`~pymatgen.core.structure.Structure`):
+            Structure as a pymatgen object
+        defect_coords (:obj:`np.array`):
+            Cartesian coordinates of the defect site
+        cutoff (:obj:`float`, optional):
+            Radius (in Angstroms) within which atomic displacements are applied.
+        stdev (:obj:`float`, optional):
+            Standard deviation (in Angstroms) of the Gaussian distribution from which atomic
+            displacement distances are drawn.
             (default: 0.25)
     Returns:
-        rattled structure"""
-    from pymatgen.core.structure import Structure
-    from pymatgen.core.sites import Site
-    
+        Rattled Structure object"""
     aaa = AseAtomsAdaptor()
     structure_copy = structure.copy()
 
-    # Classify sites in 2 lists: inside or outside 5 A sphere
+    # Classify sites in 2 lists: inside or outside cutoff sphere
     sites_inside_cutoff, sites_outside_cutoff = [], []
     for site in structure_copy:
-        distance, image = site.distance_and_image_from_frac_coords(defect_coords)[:2]
-        if distance < 5:
+        distance, _image = site.distance_and_image_from_frac_coords(defect_coords)[:2]
+        if distance < cutoff:
             sites_inside_cutoff.append(site)
         else:
             sites_outside_cutoff.append(site)
-    
-    # Apply rattle to sites within 5 A sphere
+
+    # Apply rattle to sites within cutoff sphere
     structure_inside_cutoff = structure_copy.from_sites(sites_inside_cutoff)
     ase_struct = aaa.get_atoms(structure_inside_cutoff)
     ase_struct.rattle(stdev=stdev)
     rattled_structure = aaa.get_structure(ase_struct)
-    
-    # Add the sites outside the 5 A sphere to the rattled structure
-    [ rattled_structure.append(site_outside_cutoff.specie, site_outside_cutoff.frac_coords) for site_outside_cutoff in sites_outside_cutoff ]
-    
+
+    # Add the sites outside the cutoff sphere to the rattled structure
+    for site_outside_cutoff in sites_outside_cutoff:
+        rattled_structure.append(
+            site_outside_cutoff.specie, site_outside_cutoff.frac_coords
+        )
+
     return rattled_structure
+
+
+# TODO: Add MonteCarloRattleTransformation function
