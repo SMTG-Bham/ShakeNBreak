ef1cca1e4c9d57a439a576f939a02b22b6663697
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2022-04-16 10:13:02 +0100

    Some code cleanup
    Former-commit-id: cef0daae8fb88ce2a286258bba15f8655c5f621d

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ This is a setup.py script to install shakenbreak
 
 from setuptools import setup, find_packages
 
-setup(name='shakenbreak',
-      version='0.1',
-      description='Package to generate and analyse distorted defect structures, in order to '
-                  'identify ground-state and metastable defect configurations.',
-      author='Irea Mosquera, Seán Kavanagh',
-      author_email='irea.lois.20@ucl.ac.uk',
-      packages=find_packages(),
-      license="MIT",
-      install_requires=[
+setup(
+    name="shakenbreak",
+    version="0.1",
+    description="Package to generate and analyse distorted defect structures, in order to "
+    "identify ground-state and metastable defect configurations.",
+    author="Irea Mosquera, Seán Kavanagh",
+    author_email="irea.lois.20@ucl.ac.uk",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
         "doped>=0.0.5",
         "numpy",
         "pymatgen",
@@ -22,8 +23,7 @@ setup(name='shakenbreak',
         "pandas",
         "seaborn",
         "hiphive",
+        "monty",
     ],
-    extras_require={
-        "tests": ["pytest"]      
-    },
+    extras_require={"tests": ["pytest"]},
 )
