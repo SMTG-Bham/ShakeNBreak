9daab538c060cbf6c94073b98787a73a96e8b6ea
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2022-04-17 12:14:47 +0100

    Add test for defining all possible kwargs for the mc_rattle function, and that they are correctly passed to hiphive's mc_rattle function. Add test for correct BDM behaviour when num_nearest_neighbours is greater than the number of `distorted_element` within 4.5 Angstrom, and that the correct warnings are print. Add num_nearest_neighbours = 0 test.

--- tests/test_distortions.py
+++ tests/test_distortions.py
@@ -30,6 +30,9 @@ class DistortionTestCase(unittest.TestCase):
         self.V_Cd_minus0pt5_struc_0pt1_rattled = Structure.from_file(
             os.path.join(DATA_DIR, "CdTe_V_Cd_-50%_Distortion_stdev0pt1_Rattled_POSCAR")
         )
+        self.V_Cd_minus0pt5_struc_kwarged = Structure.from_file(
+            os.path.join(DATA_DIR, "CdTe_V_Cd_-50%_Kwarged_POSCAR")
+        )
         self.Int_Cd_2_struc = Structure.from_file(
             os.path.join(DATA_DIR, "CdTe_Int_Cd_2_POSCAR")
         )
@@ -38,6 +41,9 @@ class DistortionTestCase(unittest.TestCase):
         )
         self.Int_Cd_2_minus0pt6_struc_rattled = Structure.from_file(
             os.path.join(DATA_DIR, "CdTe_Int_Cd_2_-60%_Distortion_Rattled_POSCAR")
+        )
+        self.Int_Cd_2_minus0pt6_NN_10_struc_rattled = Structure.from_file(
+            os.path.join(DATA_DIR, "CdTe_Int_Cd_2_-60%_Distortion_NN_10_POSCAR")
         )
         # Confirm correct structures and pickle dict:
         self.assertEqual(
@@ -69,9 +75,24 @@ class DistortionTestCase(unittest.TestCase):
         Distorted Neighbour Distances:\n\t[(1.42, 33, 'Te'), (1.42, 42, 'Te')]"""
         )
 
+        # Test if num_nearest_neighbours = 0 that nothing happens:
+        output = distortions.bdm(self.V_Cd_struc, 0, 0.5, frac_coords=vac_coords)
+        self.assertEqual(output["distorted_structure"], self.V_Cd_struc)
+        self.assertEqual(output["undistorted_structure"], self.V_Cd_struc)
+        self.assertEqual(output["num_distorted_neighbours"], 0)
+        np.testing.assert_array_equal(output["defect_frac_coords"], vac_coords)
+        self.assertEqual(output.get("defect_site_index"), None)
+        self.assertCountEqual(output["distorted_atoms"], [])
+        distortions.bdm(self.V_Cd_struc, 0, 0.5, frac_coords=vac_coords, verbose=True)
+        mock_print.assert_called_with(
+            f"""\tDefect Site Index / Frac Coords: {vac_coords}
+        Original Neighbour Distances: []
+        Distorted Neighbour Distances:\n\t[]"""
+        )
+
     @patch("builtins.print")
-    def test_bdm_Int_Cd_1(self, mock_print):
-        """Test bond distortion function for Int_Cd_1"""
+    def test_bdm_Int_Cd_2(self, mock_print):
+        """Test bond distortion function for Int_Cd_2"""
         site_index = 65  # Cd interstitial site index (VASP indexing)
         output = distortions.bdm(
             self.Int_Cd_2_struc, 2, 0.4, site_index=site_index, verbose=False
@@ -89,6 +110,55 @@ class DistortionTestCase(unittest.TestCase):
             f"""\tDefect Site Index / Frac Coords: {site_index}
         Original Neighbour Distances: [(2.71, 10, 'Cd'), (2.71, 22, 'Cd')]
         Distorted Neighbour Distances:\n\t[(1.09, 10, 'Cd'), (1.09, 22, 'Cd')]"""
+        )
+
+        # test correct behaviour with `num_nearest_neighbours` is greater than number of
+        # `distorted_element` atoms withing 4.5 Å of the defect site
+        output = distortions.bdm(
+            self.Int_Cd_2_struc,
+            num_nearest_neighbours=10,
+            distortion_factor=0.4,
+            distorted_element="Cd",
+            site_index=site_index,
+        )
+        self.assertEqual(
+            output["distorted_structure"], self.Int_Cd_2_minus0pt6_NN_10_struc_rattled
+        )
+        self.assertEqual(output["undistorted_structure"], self.Int_Cd_2_struc)
+        self.assertEqual(output["num_distorted_neighbours"], 10)
+        self.assertEqual(output["defect_site_index"], 65)
+        self.assertEqual(output.get("defect_frac_coords"), None)
+        self.assertCountEqual(
+            output["distorted_atoms"],
+            [
+                (10, "Cd"),
+                (22, "Cd"),
+                (29, "Cd"),
+                (1, "Cd"),
+                (14, "Cd"),
+                (24, "Cd"),
+                (30, "Cd"),
+                (38, "Te"),
+                (54, "Te"),
+                (62, "Te"),
+            ],
+        )
+        distortions.bdm(
+            self.Int_Cd_2_struc,
+            num_nearest_neighbours=10,
+            distortion_factor=0.4,
+            site_index=site_index,
+            distorted_element="Cd",
+            verbose=True,
+        )
+        mock_print.assert_called_with(
+            f"\tDefect Site Index / Frac Coords: {site_index}\n" +
+            "        Original Neighbour Distances: [(2.71, 10, 'Cd'), (2.71, 22, 'Cd'), " +
+            "(2.71, 29, 'Cd'), (4.25, 1, 'Cd'), (4.25, 14, 'Cd'), (4.25, 24, 'Cd'), (4.25, 30, " +
+            "'Cd'), (2.71, 38, 'Te'), (2.71, 54, 'Te'), (2.71, 62, 'Te')]\n" +
+            "        Distorted Neighbour Distances:\n\t[(1.09, 10, 'Cd'), (1.09, 22, 'Cd'), " +
+            "(1.09, 29, 'Cd'), (1.7, 1, 'Cd'), (1.7, 14, 'Cd'), (1.7, 24, 'Cd'), " +
+            "(1.7, 30, 'Cd'), (1.09, 38, 'Te'), (1.09, 54, 'Te'), (1.09, 62, 'Te')]"
         )
 
     def test_bdm_warnings(self):
@@ -119,11 +189,11 @@ class DistortionTestCase(unittest.TestCase):
                             self.assertEqual(w[0].category, UserWarning)
                             self.assertIn(warning_message, str(w[0].message))
                         else:
-                            self.assertEqual(len(w), 0) # No warning if we distort none
+                            self.assertEqual(len(w), 0)  # No warning if we distort none
 
         # test the case where we do have some of the `distorted_element` neighbours, but less than
         # `num_nearest_neighbours` of them with 4.5 Å of the defect site
-        for num_neighbours in range(8):
+        for num_neighbours in range(12):
             for distortion_factor in np.arange(-0.6, 0.61, 0.1):
                 with warnings.catch_warnings(record=True) as w:
                     distortions.bdm(
@@ -142,7 +212,9 @@ class DistortionTestCase(unittest.TestCase):
                         f"distort, we ignore the elemental identity. The final distortion "
                         f"information is:"
                     )
-                    if num_neighbours > 7:  # should only give warning when more than 7 distorted
+                    if (
+                        num_neighbours > 7
+                    ):  # should only give warning when more than 7 distorted
                         # neighbours requested
                         self.assertEqual(len(w), 1)
                         self.assertEqual(w[0].category, UserWarning)
@@ -179,14 +251,14 @@ class DistortionTestCase(unittest.TestCase):
             self.V_Cd_minus0pt5_struc_0pt1_rattled,
         )
 
-    def test_rattle_Int_Cd_1(self):
-        """Test structure rattle function for Int_Cd_1"""
+    def test_rattle_Int_Cd_2(self):
+        """Test structure rattle function for Int_Cd_2"""
         sorted_distances = np.sort(self.Int_Cd_2_struc.distance_matrix.flatten())
         d_min = 0.85 * sorted_distances[len(self.Int_Cd_2_struc) + 20]
 
         rattling_atom_indices = np.arange(
             0, 64
-        )  # not including index 64 which is Int_Cd_1
+        )  # not including index 64 which is Int_Cd_2
         idx = np.in1d(rattling_atom_indices, [i - 1 for i in [10, 22]])
         rattling_atom_indices = rattling_atom_indices[
             ~idx
@@ -200,6 +272,27 @@ class DistortionTestCase(unittest.TestCase):
             ),
             self.Int_Cd_2_minus0pt6_struc_rattled,
         )
+
+    def test_rattle_kwargs(self): # test all possible kwargs and explicitly check output
+        """Test structure rattle function with all possible kwargs"""
+        rattling_atom_indices = np.arange(0, 31)  # Only rattle Cd
+        sorted_distances = np.sort(self.V_Cd_struc.distance_matrix.flatten())
+        bulk_bond_length = sorted_distances[len(self.V_Cd_struc) + 20]
+        self.assertEqual(bulk_bond_length, 2.8333683853583165)
+
+        V_Cd_kwarg_rattled = distortions.rattle(
+            self.V_Cd_minus0pt5_struc,
+            stdev=0.15,
+            d_min=0.75 * bulk_bond_length,
+            nbr_cutoff=3.4,
+            n_iter=3,
+            active_atoms=rattling_atom_indices,
+            width=0.3,
+            max_attempts=10000,
+            max_disp=1.0,
+            seed=20)
+
+        self.assertEqual(V_Cd_kwarg_rattled, self.V_Cd_minus0pt5_struc_kwarged)
 
 
 if __name__ == "__main__":
