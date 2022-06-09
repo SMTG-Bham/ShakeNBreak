db9baca48e5b5e5cf130c891ccd3f44b94829d5f
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2022-06-09 14:01:29 +0100

    Fix setup.py to include data files _with pip install_ and add MANIFEST.in
    Former-commit-id: ad2566f60b22ea87c50d65c72877ac3adde48998

--- setup.py
+++ setup.py
@@ -3,11 +3,24 @@
 This is a setup.py script to install ShakeNBreak
 """
 
+import os
 from setuptools import setup, find_packages
+
+
+# https://stackoverflow.com/questions/27664504/how-to-add-package-data-recursively-in-python-setup-py
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+data_files = package_files("data/")
 
 setup(
     name="shakenbreak",
-    version="0.1",
+    version="0.2.2",
     description="Package to generate and analyse distorted defect structures, in order to "
     "identify ground-state and metastable defect configurations.",
     author="Irea Mosquera, Seán Kavanagh",
@@ -26,7 +39,5 @@ setup(
         "monty",
     ],
     extras_require={"tests": ["pytest", "pytest-mpl"]},
-    package_data={"shakenbreak": ["shakenbreak/*yaml", "shakenbreak/*txt",
-                                  "shakenbreak/*mplstyle"]},
-    data_files=["data/*"],
+    package_data={"shakenbreak": ["shakenbreak/*"] + data_files},
 )
