8488c193a3df9fee4aef99bad28896cb48e191b4
ireaml <irea1998@gmail.com>
2022-08-22 18:36:47 +0200

    fix typo in docs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Irea Mosquera-Lois, Seán R. Kavanagh'
 author = 'Irea Mosquera-Lois, Seán R. Kavanagh'
 
 # The full version, including alpha/beta/rc tags
-release = 1.0.0
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
