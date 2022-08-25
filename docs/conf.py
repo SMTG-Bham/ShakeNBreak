5e59758959ac22b4905cf242d3746549da0a1aa9
ireaml <irea1998@gmail.com>
2022-08-25 18:44:15 +0200

    update version of new release (to be compatible with pymatgen version v2022.7.25, while refactoring for compatibility with v2022.8.23 takes place)
    Former-commit-id: 0182105795e9b5c19106792617687ca31c175a17

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Irea Mosquera-Lois, Seán R. Kavanagh'
 author = 'Irea Mosquera-Lois, Seán R. Kavanagh'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
