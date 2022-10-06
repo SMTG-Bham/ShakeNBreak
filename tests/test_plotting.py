c0da9c8d742cde11856dabc443512cd608c4463b
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2022-10-06 20:28:51 +0100

    Add tests for encompassing defect naming

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -111,7 +111,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
         )
         formatted_ax = plotting._format_axis(
             ax=ax,
-            defect_name="V$_{Cd}^{0}$",
+            defect_name="$V_{Cd}^{0}$",
             y_label="Energy (eV)",
             num_nearest_neighbours=2,
             neighbour_atom="Te",
@@ -119,7 +119,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
         self.assertEqual(formatted_ax.yaxis.get_label().get_text(), "Energy (eV)")
         self.assertEqual(
             formatted_ax.xaxis.get_label().get_text(),
-            "Bond Distortion Factor (for 2 Te near V$_{Cd}^{0}$)",
+            "Bond Distortion Factor (for 2 Te near $V_{Cd}^{0}$)",
         )
         self.assertEqual(
             len(formatted_ax.yaxis.get_ticklabels()), 6 + 2
@@ -134,14 +134,14 @@ class PlottingDefectsTestCase(unittest.TestCase):
         )
         formatted_ax = plotting._format_axis(
             ax=ax,
-            defect_name="V$_{Cd}^{0}$",
+            defect_name="$V_{Cd}^{0}$",
             y_label="Energy (eV)",
             num_nearest_neighbours=2,
             neighbour_atom=None,
         )
         self.assertEqual(
             formatted_ax.xaxis.get_label().get_text(),
-            "Bond Distortion Factor (for 2 NN near V$_{Cd}^{0}$)",
+            "Bond Distortion Factor (for 2 NN near $V_{Cd}^{0}$)",
         )
         # check x label if no defect name
         ax.plot(
@@ -169,7 +169,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
         )
         semi_formatted_ax = plotting._format_axis(
             ax=ax,
-            defect_name="V$_{Cd}^{0}$",
+            defect_name="$V_{Cd}^{0}$",
             y_label="Energy (eV)",
             num_nearest_neighbours=2,
             neighbour_atom="Te",
@@ -261,7 +261,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
         )
         semi_formatted_ax = plotting._format_axis(
             ax=ax,
-            defect_name="V$_{Cd}^{0}$",
+            defect_name="$V_{Cd}^{0}$",
             y_label="Energy (eV)",
             num_nearest_neighbours=2,
             neighbour_atom="Te",
@@ -295,13 +295,13 @@ class PlottingDefectsTestCase(unittest.TestCase):
             defect_species="vac_1_Cd_0",
             include_site_num_in_name=False,
         )
-        self.assertEqual(formatted_name, "V$_{Cd}^{0}$")
+        self.assertEqual(formatted_name, "$V_{Cd}^{0}$")
         # test with site number included
         formatted_name = plotting._format_defect_name(
             defect_species="vac_1_Cd_0",
             include_site_num_in_name=True,
         )
-        self.assertEqual(formatted_name, "V$_{Cd_1}^{0}$")
+        self.assertEqual(formatted_name, "$V_{Cd_1}^{0}$")
         # test interstitial case
         formatted_name = plotting._format_defect_name(
             defect_species="Int_Cd_1_0",
@@ -321,15 +321,19 @@ class PlottingDefectsTestCase(unittest.TestCase):
             defect_species="Vac_1_Cd_0",
             include_site_num_in_name=False,
         )
-        self.assertEqual(formatted_name, "V$_{Cd}^{0}$")
+        self.assertEqual(formatted_name, "$V_{Cd}^{0}$")
 
         # check exceptions raised: invalid charge or defect_species
-        self.assertRaises(
-            ValueError,
-            plotting._format_defect_name,
-            defect_species="vac_1_Cd_a",
-            include_site_num_in_name=True,
-        )
+        # test error catching:
+        with self.assertRaises(ValueError) as e:
+            wrong_charge_error = ValueError(
+                "Problem reading defect name vac_1_Cd_a, should end with charge state after "
+                "underscore (e.g. vac_1_Cd_0)"
+            )
+            plotting._format_defect_name(defect_species = "vac_1_Cd_a", include_site_num_in_name
+            = True)
+            self.assertIn(wrong_charge_error, e.exception)
+
         self.assertRaises(
             TypeError,
             plotting._format_defect_name,
@@ -343,6 +347,56 @@ class PlottingDefectsTestCase(unittest.TestCase):
             defect_species="kk_Cd_1_0",
             include_site_num_in_name=True,
         )
+
+        defect_species_name_dict = {
+            "vac_Cd_mult32_0": "$V_{Cd}^{0}$",
+            "VSb_0": "$V_{Sb}^{0}$",
+            "VI_9": "$V_{I}^{+9}$",
+            "V_Sb_0": "$V_{Sb}^{0}$",
+            "V_I,_-2": "$V_{I}^{-2}$",
+            "V_I_-2": "$V_{I}^{-2}$",
+            "VacSb_2": "$V_{Sb}^{+2}$",
+            "VacI_2": "$V_{I}^{+2}$",
+            "Vac_Sb_3": "$V_{Sb}^{+3}$",
+            "Vac_I_1": "$V_{I}^{+1}$",
+            "VaSb_3": "$V_{Sb}^{+3}$",
+            "VaI_9": "$V_{I}^{+9}$",
+            "Va_Sb_10": "$V_{Sb}^{+10}$",
+            "Va_I_4": "$V_{I}^{+4}$",
+            "i_Sb_1": "Sb$_i^{+1}$",
+            "Sb_i_3": "Sb$_i^{+3}$",
+            "iSb_8": "Sb$_i^{+8}$",
+            "IntSb_2": "Sb$_i^{+2}$",
+            "Int_Sb_9": "Sb$_i^{+9}$",
+            "Sb_Se_9": "Sb$_{Se}^{+9}$",
+            "Sb_on_Se_9": "Sb$_{Se}^{+9}$",
+            "Int_Li_mult64_-1": "Li$_i^{-1}$",
+            "Int_Li_mult64_-2": "Li$_i^{-2}$",
+            "Int_Li_mult64_0": "Li$_i^{0}$",
+            "Int_Li_mult64_1": "Li$_i^{+1}$",
+            "Int_Li_mult64_2": "Li$_i^{+2}$",
+            "Sub_Li_on_Ni_mult32_-1": "Li$_{Ni}^{-1}$",
+            "Sub_Li_on_Ni_mult32_-2": "Li$_{Ni}^{-2}$",
+            "Sub_Li_on_Ni_mult32_0": "Li$_{Ni}^{0}$",
+            "Sub_Li_on_Ni_mult32_1": "Li$_{Ni}^{+1}$",
+            "Sub_Li_on_Ni_mult32_2": "Li$_{Ni}^{+2}$",
+            "Sub_Ni_on_Li_mult32_-1": "Ni$_{Li}^{-1}$",
+            "Sub_Ni_on_Li_mult32_-2": "Ni$_{Li}^{-2}$",
+            "Sub_Ni_on_Li_mult32_0": "Ni$_{Li}^{0}$",
+            "Sub_Ni_on_Li_mult32_1": "Ni$_{Li}^{+1}$",
+            "Sub_Ni_on_Li_mult32_2": "Ni$_{Li}^{+2}$",
+            "Vac_Li_mult32_-1": "$V_{Li}^{-1}$",
+            "Vac_Li_mult32_-2": "$V_{Li}^{-2}$",
+            "Vac_Li_mult32_0": "$V_{Li}^{0}$",
+            "Vac_Li_mult32_1": "$V_{Li}^{+1}$",
+        }
+
+        for defect_species, expected_name in defect_species_name_dict.items():
+            formatted_name = plotting._format_defect_name(
+                defect_species=defect_species,
+                include_site_num_in_name=False,
+            )
+            self.assertEqual(formatted_name, expected_name)
 
     def test_cast_energies_to_floats(self):
         """Test _cast_energies_to_floats() function."""
@@ -656,7 +710,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
             neighbour_atom=None,
             legend_label="SnB: 2 Te",
             line_color="k",
-            title="V$_{Cd}^{0}$",
+            title="$V_{Cd}^{0}$",
             save_format="svg",
             save_plot=True,
             y_label="E (eV)",
@@ -727,7 +781,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
             defect_species="vac_1_Cd_0",
             num_nearest_neighbours=2,
             neighbour_atom="Ti",
-            title="V$_{O}^{0}$",
+            title="$V_{O}^{0}$",
             save_plot=True,
             output_path=self.VASP_CDTE_DATA_DIR,
             save_format="png",
@@ -874,7 +928,7 @@ class PlottingDefectsTestCase(unittest.TestCase):
                 self.V_Cd_energies_dict_from_other_charge_states,
             ],
             dataset_labels=["SnB: 2 Te", "Another one"],
-            defect_species="V$_{Cd}^{0}$",
+            defect_species="$V_{Cd}^{0}$",
             num_nearest_neighbours=2,
             neighbour_atom="Te",
         )
