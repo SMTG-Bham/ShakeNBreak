590350e6fc70b7cfcc8139d0ed9430fbc6f6c06b
ireaml <irea1998@gmail.com>
2023-02-02 20:50:14 +0100

    fix typo
    Former-commit-id: 91fc76fbe7c05c459ca67d6d5e57d6eb7642dc09

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Irea Mosquera-Lois, Seán R. Kavanagh'
 author = 'Irea Mosquera-Lois, Seán R. Kavanagh'
 
 # The full version, including alpha/beta/rc tags
-release = '23.02.22'
+release = '23.02.02'
 
 
 # -- General configuration ---------------------------------------------------
