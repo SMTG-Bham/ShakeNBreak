d3fcccdedea4e1d604c8d7c1b8d0016574aa54db
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-02-08 16:59:10 +0000

    Fix numpy typing docs issue
    Former-commit-id: a4b6df71d70c70f90122d04abb7f6202dbadade0

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ fonts = package_files("fonts/")
 
 setup(
     name="shakenbreak",
-    version="23.02.02",
+    version="23.02.08",
     description="Package to generate and analyse distorted defect structures, in order to "
     "identify ground-state and metastable defect configurations.",
     long_description="Python package to automate the process of defect structure searching. <br>"
