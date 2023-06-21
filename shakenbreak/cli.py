5aeb9ba1ea3437574547b6173893c5e321a73484
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-06-21 18:11:47 +0100

    Add `snb-mag` check to `snb-run`

--- shakenbreak/cli.py
+++ shakenbreak/cli.py
@@ -823,8 +823,8 @@ def run(submit_command, job_script, job_name_option, all, verbose):
         job_name_option = "-N"
 
     call(
-        f"{os.path.dirname(__file__)}/bash_scripts/SnB_run.sh {optional_flags} {submit_command}"
-        f" {job_script} {job_name_option}",
+        f"{os.path.dirname(__file__)}/scripts/SnB_run.sh {optional_flags} {submit_command} {job_script} "
+        f"{job_name_option}",
         shell=True,
     )
 
