6ef4d11f2609d38183ac5d2914a64600a52f41ba
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2025-11-06 12:47:12 -0500

    Update `__version__` setting in `init.py`

--- shakenbreak/__init__.py
+++ shakenbreak/__init__.py
@@ -4,4 +4,9 @@ bond distortions to locate ground-state and metastable structures of point
 defects in solid materials.
 """
 
-__version__ = "1.0.2"
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("shakenbreak")  # from package metadata (pyproject.toml)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "No version found"  # fallback for local development or if package isn't installed
