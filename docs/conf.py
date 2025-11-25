6bf6acb999244a5b53a2d4702419b375b5a1b36d
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2025-11-25 12:35:08 -0500

    Rename logo

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ html_theme = "sphinx_book_theme" # "sphinx_rtd_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "Images/toc.png"
+html_logo = "Images/SnB_logo.png"
 html_title = "ShakeNBreak"
 
 # Add any paths that contain custom static files (such as style sheets) here,
